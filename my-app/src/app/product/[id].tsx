63ae383942a2132788c3b79c1cdfadd2f48d60c5
Matheus <matheus.oliveira199768@gmail.com>
2024-02-10 15:36:16 -0300

    Create send order to whatsapp and final adjustments

--- my-app/src/app/product/[id].tsx
+++ my-app/src/app/product/[id].tsx
@@ -4,19 +4,25 @@ import { useCartStore } from "@/stores/cart-store";
 import { PRODUCTS } from "@/utils/data/products";
 import { formatCurrency } from "@/utils/functions/format-currency";
 import { Feather } from "@expo/vector-icons";
-import { useLocalSearchParams, useNavigation } from "expo-router";
+import { Redirect, useLocalSearchParams, useNavigation } from "expo-router";
 import { Image, ScrollView, Text, View } from "react-native";
 
 export default function Product() {
   const { id } = useLocalSearchParams();
-  const cartStore = useCartStore()
-  const navigation = useNavigation()
+  const cartStore = useCartStore();
+  const navigation = useNavigation();
 
-  const product = PRODUCTS.filter((item) => item.id === id)[0];
+  const product = PRODUCTS.find((item) => item.id === id);
 
-  function handleAddToCart(){
-    cartStore.add(product)
-    navigation.goBack()
+  function handleAddToCart() {
+    if (product) {
+      cartStore.add(product);
+      navigation.goBack();
+    }
+  }
+
+  if (!product) {
+    return <Redirect href="/" />;
   }
 
   return (
@@ -28,6 +34,9 @@ export default function Product() {
           resizeMode="cover"
         />
         <View className="p-5 mt-8 flex-1">
+          <Text className="text-white text-xl font-heading">
+            {product.title}
+          </Text>
           <Text className="text-lime-400 text-2xl font-heading my-2">
             {formatCurrency(product.price)}
           </Text>
